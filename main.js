1963da8113a234f060108f99d303cba4f590e9b9
ohm_arthakorn <ohm2549arthakorn@gmail.com>
2026-02-15 11:30:41 +0700

    สร้าง Function สำหรับการทำงานเบื้องต้นอย่างการกด button เพื่อเพิ่มจำนวน

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
-let x = 10
-console.log(x)
+let count = 0;
+
+const button = document.getElementById("adding_button");
+const display_header = document.getElementById("display_header");
+    
+// สร้าง event ให้ปุ่มรอการ Interact.
+button.addEventListener('click',function(){
+    count++;
+    display_header.textContent = count;
+});
